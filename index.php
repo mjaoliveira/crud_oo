0bf9bef813984352e7f0e59cd6d3f0cb93159016
Marcelo Jorge <marcelojorge.ba@gmail.com>
2018-10-17 19:28:14 -0300

    crud_oo v2.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require 'contato.class.php';
+
+$contato = new Contato();
+
+//CREATE
+//$contato->adicionar('marcelojorge_ba@hotmail.com', 'Marcelo Jorge');
+//$contato->adicionar('fulano@gmail.com');
+
+//READ
+//$nome = $contato->getNome('marcelojorge_ba@hotmail.com');
+//echo "Nome: ".$nome;
+
+//$lista = $contato->getAll();
+
+//print_r($lista);
+
+//UPDATE
+//$contato->editar('Fulano', 'fulano@gmail.com');
+
+//DELETE
+/*$excluir = $contato->excluir('fulano@gmail.com');
+
+if ($excluir == true) {
+	echo 'Foi Excluido!';
+} else{
+	echo 'N foi excluido';
+}
+*/
+
